bb1ac4e30543fa6479dcca9d7ae54d0abe889f41
Eran Hirsch <hirscheran@gmail.com>
2021-08-20 16:39:23 +0300

    Combine QASRL-gs files

---
+++ scripts/qasrl_gs_utils.py
@@ -0,0 +1,18 @@
+from sys import argv
+from typing import Optional
+import pandas as pd
+
+
+def combine_files_tag_with_sentences(tags_file: str, sentences_file: str, combined_output_file: Optional[str] = None):
+    """
+    Transformers expect one file and there are currently two files. This method combines them
+    """
+
+    tags_df = pd.read_csv(tags_file)
+    sentences_df = pd.read_csv(sentences_file)
+    combined_df = pd.merge(tags_df, sentences_df, on="qasrl_id")
+    combined_df.to_csv(combined_output_file)
+
+
+if __name__ == "__main__":
+    combine_files_tag_with_sentences(argv[1], argv[2], argv[3])
